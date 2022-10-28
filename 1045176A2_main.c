f4657789774389bf5dbc55e3933e6e54300e0f4b
rahulsingh34 <rahulsingh342000@gmail.com>
2022-10-28 15:44:01 -0400

    setup variables

--- 1045176A2_main.c
+++ 1045176A2_main.c
@@ -5,10 +5,26 @@
 
 int main() {
 
-    /*
-    * TODO
-    * Add code here.
-    */
+    //Item count variable
+    int itemCount = 0;
+
+    //Main menu variables
+    char* mainMenuItems[] = {'Cheeseburger', 'Double Cheeseburger', 'Hotdog', 'BLT Sandwich', 'Chicken Burger'};
+    float mainMenuPrices[] = {10.99, 12,99, 8.99, 7.99, 7.99};
+
+    //Side menu variables
+    char* sideMenuItems[] = {'Fries', 'Sweet Potato Fries', 'Onion Rings'};
+    float sideMenuPrices[] = {3.49, 3.99, 3.49};
+
+    //Drink menu variables
+    char* drinkMenuItems[] = {'Lemonade', 'Iced Tea', 'Orange Juice', 'Apple Juice'};
+    float drinkMenuPrices[] = {2.49, 2.99, 2.99, 2.99};
+
+    //Dessert menu variables
+    char* dessertMenuItems[] = {'Doughnut', 'Choco Chip Cookie', 'Cinnamon Roll'};
+    float dessertMenuPrices[] = {2.49, 1.49, 2.99};
+    
+    printf("Welcome to SoCSBurger!");
 
     printf("\nEnjoy your SoCSBurger meal - have a nice day!\n\n");
     return 0;
